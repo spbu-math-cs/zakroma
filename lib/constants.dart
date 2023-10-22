0589b6d93f2856035a3a4eec37ec5e0701416180
roscherk <70962699+roscherk@users.noreply.github.com>
2023-10-22 20:30:41 +0300

    style: adjust default elevation (think — shadows) down

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 const double borderRadius = 20;
 const defaultPadding = EdgeInsets.symmetric(horizontal: 20, vertical: 8);
-const defaultElevation = 5.0;
+const defaultElevation = 4.0;
 const headlineTextAlignment = TextAlign.center;
