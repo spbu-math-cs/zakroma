aa92409676e490d075c30ea1627cbf7b7dcb2ef5
roscherk <70962699+roscherk@users.noreply.github.com>
2023-11-13 16:14:58 +0300

    feat(cart): implement change of button visibility based on scroll

--- lib/pages/cart.dart
+++ lib/pages/cart.dart
@@ -1,87 +1,110 @@
 import 'package:external_app_launcher/external_app_launcher.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../utility/flat_list.dart';
-import '../utility/rr_buttons.dart';
-import '../utility/rr_surface.dart';
+
 import '../constants.dart';
 import '../utility/custom_scaffold.dart';
+import '../utility/flat_list.dart';
 import '../utility/navigation_bar.dart';
+import '../utility/rr_buttons.dart';
+import '../utility/rr_surface.dart';
 
-class CartPage extends ConsumerWidget {
+class CartPage extends ConsumerStatefulWidget {
   const CartPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _CartPageState();
+}
+
+class _CartPageState extends ConsumerState<CartPage> {
+  bool orderButtonVisible = true;
+
+  @override
+  Widget build(BuildContext context) {
     final constants =
         ref.watch(constantsProvider(MediaQuery.of(context).size.width));
+    final ScrollController scrollController = ScrollController();
+    scrollController.addListener(() => setState(() {
+          orderButtonVisible = scrollController.position.userScrollDirection ==
+                  ScrollDirection.reverse
+              ? false
+              : true;
+        }));
+
     return CustomScaffold(
       title: 'Корзина',
       body: RRSurface(
           child: Stack(children: [
-            const FlatList(
-                separator: FlatListSeparator.rrBorder,
-                children: [
-                  Center(child: Text('Огурцы')),
-                  Center(child: Text('Помидоры')),
-                  Center(child: Text('Лук')),
-                  Center(child: Text('Огурцы')),
-                  Center(child: Text('Помидоры')),
-                  Center(child: Text('Лук')),
-                  Center(child: Text('Огурцы')),
-                  Center(child: Text('Помидоры')),
-                  Center(child: Text('Лук')),
-                ]),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                height: constants.paddingUnit * 8,
-                child: RRButton(
-                    onTap: () {
-                      showDialog(
-                          context: context,
-                          builder: (_) => AlertDialog(
-                            title: const Text('Внимание!'),
-                            content: const Text(
-                                'Вы будете перенаправлены в приложение Яндекс для оформления заказа продуктов.'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text('Назад'),
-                              ),
-                              TextButton(
-                                onPressed: () async {
-                                  await Clipboard.setData(const ClipboardData(
-                                      text:
-                                      'Закажи в лавке Огурцы короткоплодные Кураж Выборжец, помидоры, лук.'));
-                                  await LaunchApp.openApp(
-                                      androidPackageName:
-                                      'com.yandex.searchapp',
-                                      iosUrlScheme: 'https://www.icloud.com/shortcuts/560f8b7b038641519796c3311b01cd85',
-                                      appStoreLink:
-                                      'https://apps.apple.com/us/app/%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81-%D1%81-%D0%B0%D0%BB%D0%B8%D1%81%D0%BE%D0%B9/id1050704155',
-                                      openStore: true);
-                                },
-                                child: const Text('Продолжить'),
-                              ),
-                            ],
-                          ));
-                    },
-                    borderRadius: constants.dInnerRadius,
-                    padding: constants.dBlockPadding,
-                    child: Text(
-                      'Перейти к оформлению',
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium!
-                          .copyWith(fontWeight: FontWeight.bold),
-                    )),
-              ),
-            )
-          ])),
+        FlatList(
+            separator: FlatListSeparator.rrBorder,
+            scrollController: scrollController,
+            children: const [
+              Center(child: Text('Огурцы')),
+              Center(child: Text('Помидоры')),
+              Center(child: Text('Лук')),
+              Center(child: Text('Огурцы')),
+              Center(child: Text('Помидоры')),
+              Center(child: Text('Лук')),
+              Center(child: Text('Огурцы')),
+              Center(child: Text('Помидоры')),
+              Center(child: Text('Лук')),
+            ]),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            height: constants.paddingUnit * 8,
+            child: AnimatedSlide(
+              offset: orderButtonVisible ? Offset.zero : const Offset(0, 3),
+              duration: Constants.dAnimationDuration,
+              child: RRButton(
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (_) => AlertDialog(
+                              title: const Text('Внимание!'),
+                              content: const Text(
+                                  'Вы будете перенаправлены в приложение Яндекс для оформления заказа продуктов.'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Назад'),
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    await Clipboard.setData(const ClipboardData(
+                                        text:
+                                            'Закажи в лавке Огурцы короткоплодные Кураж Выборжец, помидоры, лук.'));
+                                    await LaunchApp.openApp(
+                                        androidPackageName:
+                                            'com.yandex.searchapp',
+                                        iosUrlScheme:
+                                            'https://www.icloud.com/shortcuts/560f8b7b038641519796c3311b01cd85',
+                                        appStoreLink:
+                                            'https://apps.apple.com/us/app/%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81-%D1%81-%D0%B0%D0%BB%D0%B8%D1%81%D0%BE%D0%B9/id1050704155',
+                                        openStore: true);
+                                  },
+                                  child: const Text('Продолжить'),
+                                ),
+                              ],
+                            ));
+                  },
+                  borderRadius: constants.dInnerRadius,
+                  padding: constants.dBlockPadding,
+                  child: Text(
+                    'Перейти к оформлению',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium!
+                        .copyWith(fontWeight: FontWeight.bold),
+                  )),
+            ),
+          ),
+        )
+      ])),
       bottomNavigationBar: FunctionalBottomBar(
         selectedIndex: -1, // никогда не хотим выделять никакую кнопку
         destinations: [
@@ -108,6 +131,5 @@ class CartPage extends ConsumerWidget {
         ],
       ),
     );
-
   }
 }
