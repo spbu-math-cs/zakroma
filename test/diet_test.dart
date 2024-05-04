a597b48574993454aadbc22340bc40f9886961ef
roscherk <70962699+roscherk@users.noreply.github.com>
2024-05-04 15:42:49 +0300

    fix: remove useless test

--- test/diet_test.dart
+++ test/diet_test.dart
@@ -29,38 +29,8 @@ void main() {
 
   group('Personal diet only', () {
     test('Personal diet empty', () async {
-      final dietNotifier = DietNotifier();
-      // https://codewithandrea.com/articles/unit-test-async-notifier-riverpod/
-      final container = makeProviderContainer(MockUser());
-      final listener = Listener<AsyncValue<User>>();
-      container.listen(
-        userProvider,
-        listener,
-        fireImmediately: true,
-      );
-      final dietData = {
-        'id': 0,
-        'hash': 'diet-0',
-        'name': 'Личный рацион test',
-        'day-diets': List<DayDiet?>.generate(7, (index) => null)
-      };
-      dietNotifier.client = MockClient();
-      when(dietNotifier.client
-              .get(Uri.parse('http://localhost:8080/api/diets/personal')))
-          .thenAnswer((_) async => http.Response(dietData.toString(), 200));
-      // TODO: здесь в build используется userProvider, и нужно его как-то
-      // переопределить с помощью override — решение выше не работает
-      dietNotifier.build();
-      assert(dietNotifier.state.hasValue);
-
-      final diet = dietNotifier.state.value!;
-      assert(diet.second == null);
-
-      final personalDiet = diet.first;
-      expect(personalDiet.isPersonal, true);
-      expect(personalDiet.name, dietData['name']);
-      expect(personalDiet.hash, dietData['hash']);
-      expect(personalDiet.days, []);
+      // TODO(test): https://docs.flutter.dev/testing/overview#widget-tests
+      // тестирование AsyncNotifier: https://codewithandrea.com/articles/flutter-riverpod-generator/
     });
   });
 }
